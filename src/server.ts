59237e2db1a633ba8de58a2230f2e0869a1814b8
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-05-09 19:07:55 -0300

    create a middleware for errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes'
 
@@ -8,6 +9,10 @@ app.use(express.json())
 app.use(cors())
 
 app.use(routes)
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(error)
+  res.status(500).send({ erro: error.message })
+})
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server HTTP is running!')
