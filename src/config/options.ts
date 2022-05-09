4ef1297035263b7f06875548797e3c1acb88d241
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2022-05-09 19:21:44 -0300

    Implementing Cors Options

---
+++ src/config/options.ts
@@ -0,0 +1,4 @@
+export const corsOptions = {
+  origin: 'https://nlw-return-impulse-web-alpha-six.vercel.app',
+  optionsSuccessStatus: 200
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { corsOptions } from './config/options'
 import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use(routes)
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
